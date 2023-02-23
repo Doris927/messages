cdcdf4a6db893e137cf1e5debeb8586ca8d3112d
chenmin.tang <tammytangg@gmail.com>
2023-02-24 00:44:38 +0900

    first commit

---
+++ src/messages/messages.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+
+@Controller('messages')
+export class MessagesController {
+  @Get()
+  listMessages() {
+    console.log('list messages');
+  }
+
+  @Post()
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
+
+  @Get('/:id')
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
+}
