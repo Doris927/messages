42f9b981c78731d7c7f38debf61cbf18afeb12a0
chenmin.tang <tammytangg@gmail.com>
2023-02-25 22:48:30 +0900

    about di

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,5 +1,7 @@
+import { Injectable } from '@nestjs/common';
 import { readFile, writeFile } from 'fs/promises';
 
+@Injectable()
 export class MessagesRepository {
   async findOne(id: string) {
     const contents = await readFile('messages.json', 'utf-8');
@@ -13,9 +15,11 @@ export class MessagesRepository {
     return messages;
   }
 
-  async create(message: string) {
+  async create(content: string) {
     const contents = await readFile('messages.json', 'utf-8');
     const messages = JSON.parse(contents);
     const id = Math.floor(Math.random() * 999);
+    messages[id] = { id, content };
+    await writeFile('messages.json', JSON.stringify(messages));
   }
 }
